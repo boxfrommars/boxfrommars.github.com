b24861eea13b31bec4af260239cd0d099d8da7ea
Dmitry Groza <boxfrommars@gmail.com>
2014-01-30 06:07:29 +0400

    version 0.0.1

---
+++ tux/Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        includes: {
+            build: {
+                cwd: 'src',
+                src: [ '**.htm'],
+                dest: '.',
+                options: {
+                    flatten: true,
+                    includePath: 'src'
+                }
+            }
+        },
+        watch: {
+            html: {
+                files: ['src/api/**.json', 'src/**.htm', 'src/parts/**.htm'],
+                tasks: ['includes']
+            },
+            options: {
+                livereload: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-includes');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['includes']);
+};
